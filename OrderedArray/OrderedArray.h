f436b7c8ef8f96f5b843020725c6b02bff57b2eb
EimisCode <D00239394@student.dkit.ie>
2023-08-11 11:45:53 +0100

    started question 1
    added all the basic template for the code I'm going to do for question 1

--- OrderedArray/OrderedArray.h
+++ OrderedArray/OrderedArray.h
@@ -1,5 +1,29 @@
 #pragma once
-class OrderedArray
+
+template <typename T>
+class OrderedArray 
 {
+private:
+    T* array; 
+    int size; 
+    int capacity; 
+    int grow_size; 
+
+public:
+    OrderedArray(int grow_size = 10);
+
+    ~OrderedArray();
+
+    void push(const T& newElement);
+
+    int length();
+
+    T getElement(int index);
+
+    bool remove(int index);
+
+    int search(const T& target);
+
+    void clear();
 };
 
