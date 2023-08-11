0d6e69acf0792a23051afde735e594239c507dc2
EimisCode <D00239394@student.dkit.ie>
2023-08-11 20:03:41 +0100

    created blank templates for the methods

--- OrderedArray/OrderedArray.cpp
+++ OrderedArray/OrderedArray.cpp
@@ -1,4 +1,8 @@
 // OrderedArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <iostream>
 #include "OrderedArray.h"
+int main()
+{
+
+}
--- OrderedArray/OrderedArray.h
+++ OrderedArray/OrderedArray.h
@@ -1,29 +1,77 @@
 #pragma once
 
 template <typename T>
-class OrderedArray 
+class OrderedArray
 {
 private:
-    T* array; 
-    int size; 
-    int capacity; 
-    int grow_size; 
+    T* array;
+    int size;
+    int capacity;
+    int growSize;
 
 public:
-    OrderedArray(int grow_size = 10);
-
+    OrderedArray(int grow_size);
+    OrderedArray(); 
     ~OrderedArray();
 
     void push(const T& newElement);
-
-    int length();
-
-    T getElement(int index);
-
+    int length() const;  
+    T getElement(int index) const;
     bool remove(int index);
-
-    int search(const T& target);
-
+    int search(const T& target) const;
     void clear();
-};
 
+    template <typename T>
+    OrderedArray<T>::OrderedArray(int grow_size) 
+    {
+        
+    }
+
+    template <typename T>
+    OrderedArray<T>::OrderedArray()
+    {
+        
+    }
+
+    template <typename T>
+    OrderedArray<T>::~OrderedArray()
+    {
+        delete[] this->array;
+    }
+
+    template <typename T>
+    void OrderedArray<T>::push(const T& newElement)
+    {
+      
+    }
+
+    template <typename T>
+    int OrderedArray<T>::length() const
+    {
+
+    }
+
+    template <typename T>
+    T OrderedArray<T>::getElement(int index) const
+    {
+
+    }
+
+    template <typename T>
+    bool OrderedArray<T>::remove(int index) 
+    {
+
+    }
+
+    template <typename T>
+    int OrderedArray<T>::search(const T& target) const 
+    {
+
+    }
+n
+    template <typename T>
+    void OrderedArray<T>::clear()
+    {
+
+    }
+};
